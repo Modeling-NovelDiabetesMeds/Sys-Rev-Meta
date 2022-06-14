e052055d89147437d2a82fa5bf4b1ac0b413c928
jose <jose@dohps-imac.1425mad.mountsinai.org>
2022-06-14 12:52:43 -0400

    Systematic review: cardiovascular benefits of novel diabetes drugs (log hr)

--- code/meta_loghr.R
+++ code/meta_loghr.R
@@ -11,20 +11,25 @@ library(stringr)
 library(metafor)
 library(meta)
 # Set working directory
+ 
   wd <- "~/Documents/Sys-Rev-Meta"
   setwd(wd)
 
 # A: Meta analysis and forest plots for log hazard ratios
+
 # Load data ------
-df <- read.csv("data/df.csv")
+  df <- read.csv("df.csv")
 
-# For sustGFRdecl, Leader & Sustain-6 uses a pooled result
-df <- subset(df, subset = (!(df$outcome == "sustGFRdecl" & 
-                               (df$trialname == "LEADER" | 
-                                  df$trialname == "SUSTAIN-6") ) 
-                           )
-             )
 
+
+# Soloist and Scored with divergent measure for HHF  
+  df <- subset(df, !( (trialname == "SOLOIST-WHF"& df$outcome == "HospHeartFailure") 
+                     |( df$trialname == "SCORED" & df$outcome == "HospHeartFailure")
+                     )
+               )
+  # Pooled result for leader/sustain
+  df <- subset(df, subset = !(df$trialname == "LEADER/SUSTAIN-6"))
+  
 # Compute some variables of interest ------
 
 # Log variance for HR, based on CI
@@ -40,7 +45,7 @@ df$outcomen <- ifelse(df$outcome == "allcauseMort", "All-cause Mortality",
                       df$outcomen)
 df$outcomen <- ifelse(df$outcome == "CVMort", "Cardiovascular Mortality",
                       df$outcomen)
-df$outcomen <- ifelse(df$outcome == "HospHeartFailure", "Hospitalisation for Heart Failure",
+df$outcomen <- ifelse(df$outcome == "HospHeartFailure", "Hospitalization for Heart Failure",
                       df$outcomen)
 df$outcomen <- ifelse(df$outcome == "stroke", "Stroke",
                       df$outcomen)
@@ -53,16 +58,19 @@ outcomes <- unique(df$outcome) #vector of outcomes
 df$Class <- ifelse(df$type == "GLP1", "GLP-1RAs", "SGLT2i")
 
 # Meta analysis by outcome/drugclass (loghr)
+l.mg <- list()
+l.ms <- list()
 for(i in 1:length(outcomes)){
   # I: Subset data per outcome/drugclass
   d.o <- subset(df, subset = (df$outcome == outcomes[i]))
-  d.g <- subset(d.o, subset = (Class == "GLP1-RAs"))
+  d.g <- subset(d.o, subset = (Class == "GLP-1RAs"))
   d.s <- subset(d.o, subset = (Class == "SGLT2i"))
   # II. Metagen for each outcome/class
   # II.1  model.g for GLP-1RA
-  model.g <- metagen(TE         = d.g$loghr,
+  l.mg[[i]] <- metagen(TE         = d.g$loghr,
                      seTE         = d.g$logsei,
                      data         = d.g,
+                     method.ci = "z",
                      sm           = "HR",
                      studlab      = d.g$trialname,
                      fixed        = FALSE,
@@ -73,9 +81,10 @@ for(i in 1:length(outcomes)){
                      title        = "study weights"
   )
   # II.2 model.s for SGLT2i
-  model.s <- metagen(TE           = d.s$loghr,
+  l.ms[[i]] <- metagen(TE           = d.s$loghr,
                      seTE         = d.s$logsei,
                      data         = d.s,
+                     method.ci = "t",
                      sm           = "HR",
                      studlab      = d.s$trialname,
                      fixed        = FALSE,
@@ -87,12 +96,15 @@ for(i in 1:length(outcomes)){
   )
   # III. Forest plot
   # III.a GLP-1Ra
-  # png(paste0("output/meta_loghr_glp1_", outcomes[i],".png"), 
+  # png(paste0("_forest/meta_loghr_glp1_", outcomes[i],".png"), 
   #    width = 6, height = 4, units = 'in', res = 300)  
-  forest(model.g, 
+  par(mfrow = c(2,2))
+  forest(l.mg[[i]], 
          atransf  = log, 
          overall = T,
          studlab  = TRUE,
+         effect = TRUE,
+         predict = FALSE,
          fontsize = 9,
          xlim     = c(0.2,2.6),
          at       = c(0.5,0.75,1,1.5),
@@ -100,23 +112,26 @@ for(i in 1:length(outcomes)){
          ref      = 1,
          level    = 0.95,
          weight.subgroup = "weight",
-         smlab = unique(d.o$outcomen),
-         col.diamond = "slategray4",
-         col.square  = "slategray2",
+         smlab = "",
+         col.diamond = "slategray2",
+         col.square  = "slategray4",
          leftcols = c("studlab"),
+         leftlabs = paste0(unique(d.o$outcomen), "\nGLP-1RAs"),
          label = T,
          print.Q = T, print.pval.Q = T,
          print.I2 = T,
          print.tau2 = F,
          fs.hetstat = 7,
-         text.addline1 =  "GLP-1RA",
          subgroup=F
-  )
-  dev.off()
+         )
+  # dev.off()
   # III.b GLP-1Ra    
-  # png(paste0("output/meta_loghr_sglt2i_", outcomes[i],".png"), 
+  # png(paste0("_forest/meta_loghr_sglt2i_", outcomes[i],".png"), 
   #     width = 6, height = 4, units = 'in', res = 300)  
-  forest(model.s, 
+
+   
+
+  forest(l.ms[[i]], 
          atransf  = log, 
          overall = T,
          studlab  = TRUE,
@@ -127,20 +142,31 @@ for(i in 1:length(outcomes)){
          ref      = 1,
          level    = 0.95,
          weight.subgroup = "weight",
-         smlab = unique(d.o$outcomen),
+         smlab = "",
          col.diamond = "slategray4",
          col.square  = "slategray2",
          leftcols = c("studlab"),
+         leftlabs ="\nSGLT2i",
          label = T,
          print.Q = T, print.pval.Q = T,
          print.I2 = T,
          print.tau2 = F,
          fs.hetstat = 7,
-         text.addline1 =  "SGLT2i",
+  #      text.addline1 =  "SGLT2i",
          subgroup=F
   )
-  dev.off()
+  #dev.off()
   
 }
+
+
+
+
+
+       
+
+
+
+
 
 
